680218a54c7bf8fcdaa6de1be09d65a8c627477d
activeti <tiizuka119@gmail.com>
2018-02-15 18:05:34 +0900

    extra draft

--- blog/models.py
+++ blog/models.py
@@ -11,8 +11,9 @@ class Post (models.Model):  # クラス名は大文字ではじめます
     published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
-        self.publishd_date = timezone.now()
+        self.published_date = timezone.now()
         self.save()  # データベースの値を更新するメソッド
+        print(self.published_date)
     
     def __str__(self):
         return self.title
