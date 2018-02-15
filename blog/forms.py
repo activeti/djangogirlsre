898c3952a5c26dd9f094f167e7b7803d7292a009
activeti <tiizuka119@gmail.com>
2018-02-15 17:46:52 +0900

    finish tutorial

--- blog/forms.py
+++ blog/forms.py
@@ -7,4 +7,6 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text',)
-
+        # そのままだとTextfieldのデフォルトがtextareaの40colsのため
+        # widgetsを使ってTextInputに変更
+        widgets = {'title': forms.TextInput()}
